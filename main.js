535b97a424d51985e63b976cd2704a129bb9f31e
Erik Modica <erikmodica@gmail.com>
2021-02-18 16:49:41 -0800

    add a boolean to check if the car is on, then modify the ignite function to stop the car if it is on.

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 var $car = document.querySelector('.car');
 var intervalID = null;
-// var carIsOn = false;
-
-addEventListener('keydown', changeDirection);
-addEventListener('keydown', ignitionOn);
-addEventListener('keydown', ignitionOff);
+var carIsOn = false;
 
 function changeDirection(event) {
   switch (event.key) {
@@ -29,14 +25,56 @@ function moveCar() {
   $car.style.left = num + 'px';
 }
 
-function ignitionOn(event) {
-  if (event.key === ' ') {
+function ignite(event) {
+  if (!carIsOn && event.key === ' ') {
     intervalID = setInterval(moveCar, 16);
+    carIsOn = true;
+  } else {
+    clearInterval(intervalID);
+    carIsOn = false;
   }
 }
 
-function ignitionOff(event) {
-  if (event.key === ' ') {
-    clearInterval(intervalID);
-  }
-}
+addEventListener('keydown', changeDirection);
+addEventListener('keydown', ignite);
+
+// function Car() {
+//   this.carIsOn = false;
+//   this.xAxis = 0;
+//   this.intervalID = null;
+// }
+
+// var fiesta = new Car();
+
+// Car.prototype.changeDirection = function(event) {
+//   switch (event.key) {
+//     case 'ArrowUp':
+//       $car.className = 'car up';
+//       break;
+//     case 'ArrowLeft':
+//       $car.className = 'car left';
+//       break;
+//     case 'ArrowDown':
+//       $car.className = 'car down';
+//       break;
+//     case 'ArrowRight':
+//       $car.className = 'car right';
+//       break;
+//   }
+// }
+
+// Car.prototype.moveCar = function() {
+//   console.log('yo xan')
+//   this.xAxis += 5;
+//   $car.style.left = this.xAxis + 'px';
+// }
+
+// console.log(fiesta.moveCar)
+
+// Car.prototype.ignitionOn = function(event) {
+//   if (!this.carIsOn && event.key === ' ') {
+//     console.log(this.carIsOn);
+//     this.intervalID = setInterval(this.moveCar, 16);
+//     this.carIsOn = true;
+//   }
+// }
